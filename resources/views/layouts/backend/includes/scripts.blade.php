4bd84e927854dac9eef8632955956a2692bd1474
Pranaa2004 <pranavanthayaparan4@gmail.com>
2025-09-22 13:14:29 +0530

    Add sass , dist , aset file for backend

--- resources/views/layouts/backend/includes/scripts.blade.php
+++ resources/views/layouts/backend/includes/scripts.blade.php
@@ -1,20 +1,27 @@
 <div>
-    <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
-    <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- apps -->
-    <!-- apps -->
-    <script src="../dist/js/app-style-switcher.js"></script>
-    <script src="../dist/js/feather.min.js"></script>
-    <script src="../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-    <script src="../dist/js/sidebarmenu.js"></script>
-    <!--Custom JavaScript -->
-    <script src="../dist/js/custom.min.js"></script>
-    <!--This page JavaScript -->
-    <script src="../assets/extra-libs/c3/d3.min.js"></script>
-    <script src="../assets/extra-libs/c3/c3.min.js"></script>
-    <script src="../assets/libs/chartist/dist/chartist.min.js"></script>
-    <script src="../assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
-    <script src="../assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>
-    <script src="../assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>
-    <script src="../dist/js/pages/dashboards/dashboard1.min.js"></script>
+    @vite([
+        'resources/assets/libs/jquery/dist/jquery.min.js',
+        'resources/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js',
+        //<!-- apps -->
+        'resources/dist/js/app-style-switcher.js',
+        'resources/dist/js/feather.min.js',
+        'resources/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js',
+        'resources/dist/js/sidebarmenu.js',
+        // <!--Custom JavaScript -->
+        'resources/dist/js/custom.min.js',
+        // <!--This page JavaScript -->
+        'resources/assets/extra-libs/c3/d3.min.js',
+        'resources/assets/extra-libs/c3/c3.min.js',
+        'resources/assets/libs/chartist/dist/chartist.min.js',
+        'resources/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js',
+        'resources/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js',
+        'resources/assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js',
+        'resources/dist/js/pages/dashboards/dashboard1.min.js',
+        // 'resources/',
+        // 'resources/',
+        // 'resources/',
+        // 'resources/',
+        // 'resources/',
+        // 'resources/',
+    ])
 </div>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,34 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/frontend/css/app.css', 'resources/frontend/js/app.js'],
+            input: [
+                // <---------- CSS -------------->
+                'resources/frontend/css/app.css',
+                'resources/assets/extra-libs/c3/c3.min.css',
+                'resources/assets/libs/chartist/dist/chartist.min.css',
+                'resources/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css',
+                'resources/dist/css/style.min.css',
+
+                //<---------- JS ---------------->
+                'resources/frontend/js/app.js',
+                'resources/assets/libs/jquery/dist/jquery.min.js',
+                'resources/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js',
+                //<!-- apps -->
+                'resources/dist/js/app-style-switcher.js',
+                'resources/dist/js/feather.min.js',
+                'resources/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js',
+                'resources/dist/js/sidebarmenu.js',
+                // <!--Custom JavaScript -->
+                'resources/dist/js/custom.min.js',
+                // <!--This page JavaScript -->
+                'resources/assets/extra-libs/c3/d3.min.js',
+                'resources/assets/extra-libs/c3/c3.min.js',
+                'resources/assets/libs/chartist/dist/chartist.min.js',
+                'resources/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js',
+                'resources/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js',
+                'resources/assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js',
+                'resources/dist/js/pages/dashboards/dashboard1.min.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
