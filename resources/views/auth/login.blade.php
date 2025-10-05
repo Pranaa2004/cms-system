7e0f55ec0a095d7313c666c23dd64f3237c88b25
Pranaa2004 <pranavanthayaparan4@gmail.com>
2025-10-05 17:43:36 +0530

    Convert login and register views to standalone HTML
    Replaces Blade template inheritance in login and register views with standalone HTML documents. Adds necessary meta tags, CSS/JS includes, and preloader markup for consistency with other pages. Moves script includes and password toggle logic directly into the HTML files.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,78 +1,127 @@
-@extends('layouts.backend.main')
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
 
-@section('title', 'Login')
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- Favicon icon -->
+        <link rel="icon" type="image/png" sizes="16x16"
+            href="{{ Vite::asset('resources/backend/assets/images/logo/titlelogos.png') }}">
+        <title>Login</title>
+        <!-- Custom CSS -->
+        @vite(['resources/backend/assets/extra-libs/c3/c3.min.css', 'resources/backend/assets/libs/chartist/dist/chartist.min.css', 'resources/backend/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css', 'resources/backend/dist/css/style.min.css'])
+        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> --}}
+        <!-- Custom CSS -->
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+    </head>
 
-@section('LogReg')
-    <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
-        style="repeat center center;">
-        <div class="auth-box row">
-            <!-- Left Image Column -->
-            <div class="col-lg-7 col-md-5 modal-bg-img"
-                style="background-image: url({{ Vite::asset('resources/backend/assets/images/logo/1.png') }});">
+    <body>
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
             </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Main wrapper - style you can find in pages.scss -->
+        <!-- ============================================================== -->
 
-            <!-- Right Form Column -->
-            <div class="col-lg-5 col-md-7 bg-white">
-                <div class="p-3">
-                    <div class="text-center">
-                        <img src="{{ Vite::asset('resources/backend/assets/images/big/icon.png') }}" alt="Admin Panel Logo">
-                    </div>
-                    <h2 class="mt-3 text-center">Sign In</h2>
 
-                    <!-- Display Session Error -->
-                    @if (session('error'))
-                        <div class="alert alert-danger">{{ session('error') }}</div>
-                    @endif
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
+            style="repeat center center;">
+            <div class="auth-box row">
+                <!-- Left Image Column -->
+                <div class="col-lg-7 col-md-5 modal-bg-img"
+                    style="background-image: url({{ Vite::asset('resources/backend/assets/images/logo/1.png') }});">
+                </div>
 
-                    <!-- Login Form -->
-                    <form class="mt-4" action="{{ route('login_store') }}" method="post">
-                        @csrf
-                        <div class="row">
-                            <!-- Username -->
-                            <div class="col-lg-12">
-                                <div class="form-group mb-3">
-                                    <label class="form-label text-dark" for="uname">Username</label>
-                                    <input class="form-control" id="uname" type="email" name="email"
-                                        placeholder="Enter your username" required>
-                                    @error('email')
-                                        <small class="text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <!-- Password -->
-                            <div class="col-lg-12">
-                                <div class="form-group mb-3">
-                                    <label class="form-label text-dark" for="pwd">Password</label>
-                                    <input class="form-control" id="pwd" type="password" name="password"
-                                        placeholder="Enter your password" required autocomplete="off">
-                                    @error('password')
-                                        <small class="text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <!-- Remember Me -->
-                            <div class="col-lg-12">
-                                <div class="form mb-3">
-                                    <a href="{{ route('forgotpw') }}" class="fs-6 text-start">Forgot Password</a>
-                                </div>
-                            </div>
-
-                            <!-- Submit Button -->
-                            <div class="col-lg-12 text-center">
-                                <button type="submit" class="btn w-100 btn-dark">Sign In</button>
-                            </div>
-
-                            <!-- Register Link -->
-                            <div class="col-lg-12 text-center mt-5">
-                                Don't have an account?
-                                <a href="{{ route('register_show') }}" class="text-danger">Sign Up</a>
-                            </div>
+                <!-- Right Form Column -->
+                <div class="col-lg-5 col-md-7 bg-white">
+                    <div class="p-3">
+                        <div class="text-center">
+                            <img src="{{ Vite::asset('resources/backend/assets/images/big/icon.png') }}"
+                                alt="Admin Panel Logo">
                         </div>
-                    </form>
+                        <h2 class="mt-3 text-center">Sign In</h2>
+
+                        <!-- Display Session Error -->
+                        @if (session('error'))
+                            <div class="alert alert-danger">{{ session('error') }}</div>
+                        @endif
+
+                        <!-- Login Form -->
+                        <form class="mt-4" action="{{ route('login_store') }}" method="post">
+                            @csrf
+                            <div class="row">
+                                <!-- Username -->
+                                <div class="col-lg-12">
+                                    <div class="form-group mb-3">
+                                        <label class="form-label text-dark" for="uname">Username</label>
+                                        <input class="form-control" id="uname" type="email" name="email"
+                                            placeholder="Enter your username" required>
+                                        @error('email')
+                                            <small class="text-danger">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <!-- Password -->
+                                <div class="col-lg-12">
+                                    <div class="form-group mb-3">
+                                        <label class="form-label text-dark" for="pwd">Password</label>
+                                        <input class="form-control" id="pwd" type="password" name="password"
+                                            placeholder="Enter your password" required autocomplete="off">
+                                        @error('password')
+                                            <small class="text-danger">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <!-- Remember Me -->
+                                <div class="col-lg-12">
+                                    <div class="form mb-3">
+                                        <a href="{{ route('forgotpw') }}" class="fs-6 text-start">Forgot Password</a>
+                                    </div>
+                                </div>
+
+                                <!-- Submit Button -->
+                                <div class="col-lg-12 text-center">
+                                    <button type="submit" class="btn w-100 btn-dark">Sign In</button>
+                                </div>
+
+                                <!-- Register Link -->
+                                <div class="col-lg-12 text-center mt-5">
+                                    Don't have an account?
+                                    <a href="{{ route('register_show') }}" class="text-danger">Sign Up</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+        <!-- ============================================================== -->
+        <!-- End Wrapper -->
+        <!-- ============================================================== -->
+        <!-- End Wrapper -->
+        <!-- ============================================================== -->
+        <!-- All Jquery -->
+        <!-- ============================================================== -->
+        @include('layouts.backend.includes.scripts')
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        
+    </body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,43 @@
-@extends('layouts.backend.main')
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
 
-@section('title', 'Register')
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- Favicon icon -->
+        <link rel="icon" type="image/png" sizes="16x16"
+            href="{{ Vite::asset('resources/backend/assets/images/logo/titlelogos.png') }}">
+        <title>@yield('title')</title>
+        <!-- Custom CSS -->
+        @vite(['resources/backend/assets/extra-libs/c3/c3.min.css', 'resources/backend/assets/libs/chartist/dist/chartist.min.css', 'resources/backend/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css', 'resources/backend/dist/css/style.min.css'])
+        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> --}}
+        <!-- Custom CSS -->
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+    </head>
 
-@section('LogReg')
-    <!-- Login box.scss -->
-    <!-- ============================================================== -->
-    <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
+    <body>
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Main wrapper - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+         <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
         style="background:url({{ Vite::asset('resources/backend//assets/image/big/auth-bg.jpg') }}) no-repeat center center;">
         <div class="auth-box row text-center">
             <div class="col-lg-7 col-md-5 modal-bg-img"
@@ -64,78 +96,83 @@
             </div>
         </div>
     </div>
-    </div>
-    <!-- ============================================================== -->
-    <!-- Login box.scss -->
-@endsection
+    </div>n
+        <!-- ============================================================== -->
+        <!-- End Wrapper -->
+        <!-- ============================================================== -->
+        <!-- End Wrapper -->
+        <!-- ============================================================== -->
+        <!-- All Jquery -->
+        <!-- ============================================================== -->
+        @include('layouts.backend.includes.scripts')
+        <script>
+            // $(document).ready(function() {
+            document.addEventListener('DOMContentLoaded', function() {
+                const passwordField = document.getElementById('password');
+                const togglePassword = document.getElementById('togglePassword');
+                const eyeIcon = document.getElementById('eyeIcon');
 
-@push('script')
-    <script>
-        // $(document).ready(function() {
-        document.addEventListener('DOMContentLoaded', function() {
-            const passwordField = document.getElementById('password');
-            const togglePassword = document.getElementById('togglePassword');
-            const eyeIcon = document.getElementById('eyeIcon');
+                const passwordField2 = document.getElementById('confirm_password');
+                const togglePassword2 = document.getElementById('togglePassword2');
+                const eyeIcon2 = document.getElementById('eyeIcon2');
 
-            const passwordField2 = document.getElementById('confirm_password');
-            const togglePassword2 = document.getElementById('togglePassword2');
-            const eyeIcon2 = document.getElementById('eyeIcon2');
+                togglePassword.addEventListener('click', function() {
+                    // Toggle the type attribute
+                    const type = passwordField.getAttribute('type') === 'password' ? 'text' :
+                        'password';
+                    passwordField.setAttribute('type', type);
 
-            togglePassword.addEventListener('click', function() {
-                // Toggle the type attribute
-                const type = passwordField.getAttribute('type') === 'password' ? 'text' :
-                    'password';
-                passwordField.setAttribute('type', type);
-
-                // Toggle the eye icon
-                if (type === 'password') {
-                    eyeIcon.classList.remove('fa-eye');
-                    eyeIcon.classList.add('fa-eye-slash');
+                    // Toggle the eye icon
+                    if (type === 'password') {
+                        eyeIcon.classList.remove('fa-eye');
+                        eyeIcon.classList.add('fa-eye-slash');
 
 
-                } else {
-                    eyeIcon.classList.remove('fa-eye-slash');
-                    eyeIcon.classList.add('fa-eye');
+                    } else {
+                        eyeIcon.classList.remove('fa-eye-slash');
+                        eyeIcon.classList.add('fa-eye');
 
-                }
+                    }
+                });
+
+                togglePassword2.addEventListener('click', function() {
+                    // Toggle the type attribute
+                    const type = passwordField2.getAttribute('type') === 'password' ? 'text' :
+                        'password';
+                    passwordField2.setAttribute('type', type);
+
+                    // Toggle the eye icon
+                    if (type === 'password') {
+                        eyeIcon2.classList.remove('fa-eye');
+                        eyeIcon2.classList.add('fa-eye-slash');
+
+
+                    } else {
+                        eyeIcon2.classList.remove('fa-eye-slash');
+                        eyeIcon2.classList.add('fa-eye');
+
+                    }
+                });
             });
-
-            togglePassword2.addEventListener('click', function() {
-                // Toggle the type attribute
-                const type = passwordField2.getAttribute('type') === 'password' ? 'text' :
-                    'password';
-                passwordField2.setAttribute('type', type);
-
-                // Toggle the eye icon
-                if (type === 'password') {
-                    eyeIcon2.classList.remove('fa-eye');
-                    eyeIcon2.classList.add('fa-eye-slash');
+            // });
 
 
-                } else {
-                    eyeIcon2.classList.remove('fa-eye-slash');
-                    eyeIcon2.classList.add('fa-eye');
+            // $(document).ready(function() {
+            //     let password = $('#psw').val().trim();
+            //     let conpassword = $('#cpsw').val().trim();
 
-                }
-            });
-        });
-        // });
+            //     if(password == conpassword){
+            //         $('msg').val("Hello");
+            //     }
 
-
-        // $(document).ready(function() {
-        //     let password = $('#psw').val().trim();
-        //     let conpassword = $('#cpsw').val().trim();
-
-        //     if(password == conpassword){
-        //         $('msg').val("Hello");
-        //     }
-
-        //     $('#frm_register').submit(function(){
-        //         alert("jiii")
-        //        $('msg').val("Hello");
-        //     })
+            //     $('#frm_register').submit(function(){
+            //         alert("jiii")
+            //        $('msg').val("Hello");
+            //     })
 
 
-        // });
-    </script>
-@endpush
+            // });
+        </script>
+    </body>
+
+</html>
