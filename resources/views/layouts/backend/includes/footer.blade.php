86faacbcc014d8047c5c2d7e02ca46f1987d2686
Pranaa2004 <pranavanthayaparan4@gmail.com>
2025-10-08 13:50:11 +0530

    try to fix the footer issue

--- resources/views/layouts/backend/includes/footer.blade.php
+++ resources/views/layouts/backend/includes/footer.blade.php
@@ -1,6 +1,6 @@
     <!-- footer -->
     <!-- ============================================================== -->
-    <footer class="footer text-center text-muted">
+    <footer class="footer text-center text-muted mt-auto">
         All Rights Reserved by PranaW. Designed and Developed by <a href="#">PranaW </a>.
     </footer>
     <!-- ============================================================== -->
